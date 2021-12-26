b6b0aad7962292a51cab1f9c4e2bc1c2aefd5717
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2021-12-26 17:03:39 +0900

    feat: add files

---
+++ day1/make.sh
@@ -0,0 +1,2 @@
+clang -target x86_64-pc-win32--coff -mno-red-zone -fno-stack-protector -fshort-wchar -Wall -c hello.c
+lld -flavor link -subsystem:efi_application -entry:EfiMain -out:hello.efi hello.o
