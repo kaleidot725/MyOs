6ac21d1baf8e4103dc44c034cb22515bf26bf9fd
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2022-01-01 17:43:26 +0900

    feat: add includes

--- day2b/Main.c
+++ day2b/Main.c
@@ -1,5 +1,11 @@
-#include <Uefi.h>
-#include <Library/UefiLib.h>
+#include  <Uefi.h>
+#include  <Library/UefiLib.h>
+#include  <Library/UefiBootServicesTableLib.h>
+#include  <Library/PrintLib.h>
+#include  <Protocol/LoadedImage.h>
+#include  <Protocol/SimpleFileSystem.h>
+#include  <Protocol/DiskIo2.h>
+#include  <Protocol/BlockIo.h>
 
 struct MemoryMap
 {
