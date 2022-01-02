dd2d52bce31ac57f7219e4e087a7373bfc42aed9
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2022-01-02 22:14:34 +0900

    feat: fix day3 sample

--- day3a/MikanLoaderPkg/Main.c
+++ day3a/MikanLoaderPkg/Main.c
@@ -1,11 +1,12 @@
-#include <Uefi.h>
-#include <Library/UefiLib.h>
-#include <Library/UefiBootServicesTableLib.h>
-#include <Library/PrintLib.h>
-#include <Protocol/LoadedImage.h>
-#include <Protocol/SimpleFileSystem.h>
-#include <Protocol/DiskIo2.h>
-#include <Protocol/BlockIo.h>
+#include  <Uefi.h>
+#include  <Library/UefiLib.h>
+#include  <Library/UefiBootServicesTableLib.h>
+#include  <Library/PrintLib.h>
+#include  <Protocol/LoadedImage.h>
+#include  <Protocol/SimpleFileSystem.h>
+#include  <Protocol/DiskIo2.h>
+#include  <Protocol/BlockIo.h>
+#include  <Guid/FileInfo.h>
 
 struct MemoryMap
 {
