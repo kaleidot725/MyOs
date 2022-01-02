c5fe5535053458d1b4695c1792efa27ddc7a539a
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2022-01-02 22:57:50 +0900

    feat: fix Main.c

--- day3a/MikanLoaderPkg/Main.c
+++ day3a/MikanLoaderPkg/Main.c
@@ -2,7 +2,6 @@
 #include  <Library/UefiLib.h>
 #include  <Library/UefiBootServicesTableLib.h>
 #include  <Library/PrintLib.h>
-#include  <Library/MemoryAllocationLib.h>
 #include  <Protocol/LoadedImage.h>
 #include  <Protocol/SimpleFileSystem.h>
 #include  <Protocol/DiskIo2.h>
@@ -132,30 +131,6 @@ EFI_STATUS OpenRootDir(EFI_HANDLE image_handle, EFI_FILE_PROTOCOL **root)
     return EFI_SUCCESS;
 }
 
-EFI_STATUS OpenGOP(EFI_HANDLE image_handle,
-                   EFI_GRAPHICS_OUTPUT_PROTOCOL** gop) {
-  UINTN num_gop_handles = 0;
-  EFI_HANDLE* gop_handles = NULL;
-  gBS->LocateHandleBuffer(
-      ByProtocol,
-      &gEfiGraphicsOutputProtocolGuid,
-      NULL,
-      &num_gop_handles,
-      &gop_handles);
-
-  gBS->OpenProtocol(
-      gop_handles[0],
-      &gEfiGraphicsOutputProtocolGuid,
-      (VOID**)gop,
-      image_handle,
-      NULL,
-      EFI_OPEN_PROTOCOL_BY_HANDLE_PROTOCOL);
-
-  FreePool(gop_handles);
-
-  return EFI_SUCCESS;
-}
-
 EFI_STATUS EFIAPI UefiMain(EFI_HANDLE image_handle, EFI_SYSTEM_TABLE *system_table)
 {
     // memmap の取得
@@ -171,25 +146,6 @@ EFI_STATUS EFIAPI UefiMain(EFI_HANDLE image_handle, EFI_SYSTEM_TABLE *system_tab
 
     SaveMemoryMap(&memmap, memmap_file);
     memmap_file->Close(memmap_file);
-
-    // GOP (Graphics Output Protocol) 
-    EFI_GRAPHICS_OUTPUT_PROTOCOL* gop;
-    OpenGOP(image_handle, &gop);
-    Print(L"Resolution: %ux%u, Pixel Format: %s, %u pixels/line\n", 
-        gop->Mode->Info->HorizontalResolution, 
-        gop->Mode->Info->VerticalResolutin, 
-        GetPixelFormatUnicode(gop->Mode->Info->PixelFormat), 
-        gop->Mode->Info->PixelsPerScanLine
-    );
-    Print(L"Frame Buffer: 0x%0lx - 0x%0lx, Size: %lu bytes\n", 
-        gop->Mode->FrameBufferBase,
-        gop->Mode->FrameBufferBase + gop->Mode->FrameBufferSize,
-        gop->Mode->FrameBufferSize
-    );
-    UINT8* frame_buffer = (UINT8*)gop->Mode->FrameBufferBase;
-    for (UINTN i = 0; i < gop->Mode->FrameBufferSize; ++i) {
-        frame_buffer[i] = 255;
-    }
 
     // カーネルの読み込み
     EFI_FILE_PROTOCOL* kernel_file;
