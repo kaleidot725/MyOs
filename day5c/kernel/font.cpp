b6e935cc83d1912000e4a96f7028b63b143efc6f
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2022-05-07 14:07:08 +0900

    Update day5c

--- day5c/kernel/font.cpp
+++ day5c/kernel/font.cpp
@@ -6,7 +6,7 @@ extern const uint8_t _binary_hankaku_bin_size;
 
 const uint8_t* GetFont(char c) {
     auto index = 16 * static_cast<unsigned int>(c);
-    if (index >= reinterpret_cast<unintptr_t>(&_binary_hankaku_bin_size)) {
+    if (index >= reinterpret_cast<uintptr_t>(&_binary_hankaku_bin_size)) {
         return nullptr;
     }
     return &_binary_hankaku_bin_start + index;
